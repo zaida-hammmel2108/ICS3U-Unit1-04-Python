2da120211c5916dab3e647dc57f7ba37afb6d107
Zaida Hammel <zzaida.hammel@mths.ca>
2022-09-19 21:10:49 +0000

    added rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Zaida Hammel
+# Created on: Sept 2022
+# Calculates area and perimeter of a rectangle'
+
+
+def main():
+    print("A rectangle has the dimensions: ")
+    print("4cm x 3cm")
+    print("")
+    print("Area is {}cm².".format(4 * 3))
+    print("Perimeter is {}cm.".format(2 * (4 + 3)))
+
+    
+if __name__ == "__main__":
+    main()
